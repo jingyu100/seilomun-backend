7dca14d1758404b0ba92d6e9b800b564575fd2f4
jingyu100 <jingyu100@naver.com>
2025-03-05 18:45:52 +0900

    Initialize Spring Boot project

---
+++ seilomun/src/main/java/com/yju/team2/seilomun/SeilomunApplication.java
@@ -0,0 +1,13 @@
+package com.yju.team2.seilomun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeilomunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeilomunApplication.class, args);
+	}
+
+}
---
+++ seilomun/src/test/java/com/yju/team2/seilomun/SeilomunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yju.team2.seilomun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeilomunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
