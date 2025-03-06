1e809768efd8c2f06def801526eff1f196f86360
jingyu100 <jingyu100@naver.com>
2025-03-06 14:29:43 +0900

    add Customer and Seller entities

---
+++ seilomun/src/main/java/com/yju/team2/seilomun/entity/Customer.java
@@ -0,0 +1,72 @@
+package com.yju.team2.seilomun.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cu_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 10)
+    private String name;
+
+    @Column(name = "phone", nullable = false, length = 11)
+    private String phone;
+
+    @Column(name = "nickname", nullable = false, length = 10, unique = true)
+    private String nickname;
+
+    @Column(name = "birth_date", nullable = false, length = 4)
+    private String birthDate;
+
+    @Column(name = "gender", nullable = false, length = 1)
+    private Character gender;
+
+    @Column(name = "profile_image_url", nullable = false)
+    private String profileImageUrl;
+
+    @Column(name = "points", nullable = false)
+    private Integer points;
+
+    @Column(name = "status", nullable = false, length = 1)
+    private Character status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Order> orders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Address> addresses = new ArrayList<>();
+
+}
