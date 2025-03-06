c0448e7450a3c241b12837b3b47c012957fa17e9
MoonJihoon5788 <munjihun3824@naver.com>
2025-03-06 15:52:52 +0900

    레파지토리추가

---
+++ seilomun/src/main/java/com/yju/team2/seilomun/repository/SellerPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.yju.team2.seilomun.repository;
+
+import com.yju.team2.seilomun.entity.SellerPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerPhotoRepository extends JpaRepository<SellerPhoto, Long> {
+}
