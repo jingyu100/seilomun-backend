3e8269d9fff869092c605bf8e58cd48a6bde3290
MoonJihoon5788 <munjihun3824@naver.com>
2025-03-17 19:14:08 +0900

    소비자 회원가입 로그인

--- seilomun/src/main/java/com/yju/team2/seilomun/api/user/UserController.java
+++ seilomun/src/main/java/com/yju/team2/seilomun/api/user/UserController.java
@@ -3,12 +3,17 @@ package com.yju.team2.seilomun.api.user;
 import com.yju.team2.seilomun.domain.customer.entity.Customer;
 import com.yju.team2.seilomun.domain.customer.service.CustomerService;
 import com.yju.team2.seilomun.dto.ApiResponseJson;
+import com.yju.team2.seilomun.dto.CustomerLoginDto;
 import com.yju.team2.seilomun.dto.CustomerRegisterDto;
+import com.yju.team2.seilomun.dto.SellerLoginDto;
 import com.yju.team2.seilomun.util.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +28,7 @@ public class UserController {
 
     private final CustomerService customerService;
 
-    @PostMapping("/api/account/register")
+    @PostMapping("/customer/join")
     public ApiResponseJson registerNewAccount(@Valid @RequestBody CustomerRegisterDto customerRegisterDto,
                                               BindingResult bindingResult) {
 
@@ -39,4 +44,43 @@ public class UserController {
                 "username", customer.getName()
         ));
     }
+
+    @PostMapping("/customer/login")
+    public ResponseEntity<ApiResponseJson> customerLogin(@Valid @RequestBody CustomerLoginDto customerLoginDto,
+                                                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new IllegalArgumentException("잘못된 요청입니다.");
+        }
+
+        String token = customerService.customerLogin(customerLoginDto);
+        log.info("Account successfully login with token: {}.", token);
+
+        ResponseCookie cookie = getResponseCookie(token);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString()) // 쿠키를 응답 헤더에 추가
+                .body(new ApiResponseJson(HttpStatus.OK, Map.of(
+                        "token", token,
+                        "message", "로그인 성공"
+                )));
+    }
+
+    // 쿠키에 토큰 삽입
+    private static ResponseCookie getResponseCookie(String token) {
+        // HttpOnly & Secure 쿠키 설정
+        ResponseCookie cookie = ResponseCookie.from("Authorization", token)
+                .httpOnly(true)  // JavaScript에서 접근 불가능
+                .secure(true)    // HTTPS에서만 전송
+                .sameSite("None") // CSRF 방지를 위한 SameSite 설정
+                /*
+                Strict = 다른 사이트에서 요청할 때 쿠키가 전송되지 않음
+                Lax = GET 요청 같은 안전한 요청에서는 쿠키 전송 가능
+                None = 크로스 사이트 요청에서도 쿠키 사용 가능(HTTPS 필수)
+                * */
+                .path("/")       // 모든 경로에서 쿠키 사용 가능
+                .maxAge(30 * 60 * 4) // 2시간 유지
+                .build();
+        return cookie;
+    }
+
 }
--- seilomun/src/main/java/com/yju/team2/seilomun/domain/customer/service/CustomerService.java
+++ seilomun/src/main/java/com/yju/team2/seilomun/domain/customer/service/CustomerService.java
@@ -1,14 +1,20 @@
 package com.yju.team2.seilomun.domain.customer.service;
 
+import com.yju.team2.seilomun.domain.auth.RefreshTokenService;
 import com.yju.team2.seilomun.domain.customer.entity.Customer;
 import com.yju.team2.seilomun.domain.customer.repository.CustomerRepository;
+import com.yju.team2.seilomun.domain.seller.entity.Seller;
+import com.yju.team2.seilomun.dto.CustomerLoginDto;
 import com.yju.team2.seilomun.dto.CustomerRegisterDto;
+import com.yju.team2.seilomun.dto.SellerLoginDto;
+import com.yju.team2.seilomun.util.JwtUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @Slf4j
@@ -22,6 +28,8 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final RefreshTokenService refreshTokenService;
 
     public Customer registerCustomer(CustomerRegisterDto customerRegisterDto) {
         checkPasswordStrength(customerRegisterDto.getPassword());
@@ -54,5 +62,25 @@ public class CustomerService {
         }
         log.info("비밀번호 정책 미달");
         throw new IllegalArgumentException("비밀번호 최소 8자에 영어, 숫자, 특수문자를 포함해야 합니다.");
+    }
+
+    //소비자 로그인
+    public String customerLogin(CustomerLoginDto customerLoginDto) {
+        Optional<Customer> byEmail = customerRepository.findByEmail(customerLoginDto.getEmail());
+        if (byEmail.isEmpty()) {
+            log.info(byEmail.toString());
+            throw new IllegalArgumentException("존재하지 않는 이메일입니다.");
+        }
+        Customer customer = byEmail.get();
+        if (!passwordEncoder.matches(customerLoginDto.getPassword(), customer.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치 하지 않습니다.");
+        }
+
+        // RefreshToken 생성 및 Redis에 저장
+        String refreshToken = jwtUtil.generateRefreshToken(customer.getEmail(), "SELLER");
+        refreshTokenService.saveRefreshToken(customer.getEmail(), "SELLER", refreshToken);
+
+        // AccessToken 생성 및 반환
+        return jwtUtil.generateAccessToken(customer.getEmail(), "SELLER");
     }
 }
