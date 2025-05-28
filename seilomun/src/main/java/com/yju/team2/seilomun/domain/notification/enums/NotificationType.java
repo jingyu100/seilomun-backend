1b0b3078837929c71623142a3e35e9caa0ec9840
jingyu100 <jingyu100@naver.com>
2025-05-28 12:48:57 +0900

    Update NotificationType.java

--- seilomun/src/main/java/com/yju/team2/seilomun/domain/notification/enums/NotificationType.java
+++ seilomun/src/main/java/com/yju/team2/seilomun/domain/notification/enums/NotificationType.java
@@ -2,7 +2,7 @@ package com.yju.team2.seilomun.domain.notification.enums;
 
 public enum NotificationType {
 
-    // 소비자 입장
+    // 소비자에게 전송할 알림
     NEW_PRODUCT("즐겨찾기한 매장에서 새 상품 등록"),
     ORDER_ACCEPTED("주문 수락"),
     ORDER_DECLINED("주문 거절"),
@@ -11,7 +11,7 @@ public enum NotificationType {
     LIKE_PRODUCT_STATUS_CHANGED("좋아요한 상품 상태가 변경"),
     CART_PRODUCT_STATUS_CHANGED("장바구니 상품 상태가 변경"),
 
-    // 판매자 입장
+    // 판매자에게 전송할 알림
     ORDER_OFFERED("주문 신청"),
     ORDER_REFUND("환불 신청"),
     PRODUCT_STATUS_CHANGED("등록된 상품 상태가 변경"),
