a9a1c5fa592c7c5ccb0758c1ebb079cd1b3f4d75
jingyu100 <jingyu100@naver.com>
2025-06-12 16:58:57 +0900

    Update CorsConfig.java

--- seilomun/src/main/java/com/yju/team2/seilomun/config/CorsConfig.java
+++ seilomun/src/main/java/com/yju/team2/seilomun/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 엔드포인트에 대해 CORS 설정
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:5173")  // 허용할 origin
+                .allowedOrigins("http://localhost:5173", "http://3.36.70.70:5173")  // 허용할 origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 쿠키 및 인증 정보 허용
