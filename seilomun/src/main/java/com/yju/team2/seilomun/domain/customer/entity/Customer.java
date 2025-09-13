5e86f0590215c1541d245a560ec85fb5479d5239
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-09-13 16:04:12 +0900

    Update Customer.java

--- seilomun/src/main/java/com/yju/team2/seilomun/domain/customer/entity/Customer.java
+++ seilomun/src/main/java/com/yju/team2/seilomun/domain/customer/entity/Customer.java
@@ -72,6 +72,7 @@ public class Customer {
 
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     @Builder.Default
     private List<Order> orders = new ArrayList<>();
 
